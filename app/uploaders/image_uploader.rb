67de3453d8c678c96647cfc1117a8cbc75e05dc6
kaclar-ef <62864518+kaclar-ef@users.noreply.github.com>
2020-08-19 16:30:40 +0900

    delete files
    delete deploy javascript files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [300, 300]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
